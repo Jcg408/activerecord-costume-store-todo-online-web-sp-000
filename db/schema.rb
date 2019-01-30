b6b1f24148029793c8289982e5f317e96e3f8d46
Jessica Gladstone <jcg321@comcast.net>
2019-01-30 18:32:54 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.text "size"
     t.string "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
@@ -35,8 +37,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "theme"
     t.float "price"
     t.boolean "family_friendly"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
     t.text "description"
   end
 
