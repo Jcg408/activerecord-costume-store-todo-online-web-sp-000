e2d2223d1b78312775b0e02c47da95e53a723b53
Jessica Gladstone <jcg321@comcast.net>
2019-01-30 18:15:45 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,11 +1,12 @@
 # Create your costume_stores migration here
-class CreateCostumes < ActiveRecord::Migration[4.2]
+class CreateCostumeStores< ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
       t.float :price
       t.text :size
       t.string :image_url
+      t.timestamp
     end
   end
 end
