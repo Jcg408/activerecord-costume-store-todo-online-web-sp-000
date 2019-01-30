4bcc52e4ba49c539ed5ba19cdfefaf00d2890b7e
Jessica Gladstone <jcg321@comcast.net>
2019-01-30 18:09:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,6 +11,9 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
   create_table :costumes do |t|
-    t. string :name
+    t.string :name
+    t.float :price
+    t.size :text 
+    
   end
 end
